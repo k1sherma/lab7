a71e3ca55bd7ac130d4fff16a181e7e8f1cb4274
Kyle Sherman <k1sherma@ucsd.edu>
2018-02-22 04:39:50 -0800

    added analytics

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(likeClicked);
+}
+function likeClicked(){
+	ga("send", "event", "like", "click");
 }
